a00d2faaf1c328f88924340693183ba336ba5035
fonces <fonces.xyz@gmail.com>
2024-06-09 12:54:29 +0900

    画像パスの修正とファイルのアップロード

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -12,10 +12,14 @@ const styles = {
 }
 
 const images = [
-  {
-    src: "/img/test.png",
-    alt: "Coming Soon...",
-  },
+  ...Array(12).fill(null).map((_, index) => ({
+    src: `${process.env.NEXT_PUBLIC_BASE_PATH || ''}/img/${index + 1}.jpeg`,
+    alt: `画像${index + 1}`,
+  })),
+  // {
+  //   src: `${process.env.NEXT_PUBLIC_BASE_PATH || ''}/img/test.png`,
+  //   alt: "Coming Soon...",
+  // },
 ];
 
 export default function Gallery() {
